7788f3c95e1bad9db280963b328bb7dc805ec8cb
Adam J. Lincoln <adamjlincoln@gmail.com>
2013-12-08 00:33:57 -0500

    Build model dicts better - don't just grab ref to __dict__ (that was dumb) and recurse down one level of relationships

--- src/pydiditbackend/models/Model.py
+++ src/pydiditbackend/models/Model.py
@@ -1,15 +1,35 @@
+from sqlalchemy.inspection import inspect
+
+from pydiditbackend.models import Base
+
 class Model(object):
     '''Model object'''
 
-    def to_dict(self):
-        to_return = self.__dict__
-        if '_sa_instance_state' in to_return:
-            del to_return['_sa_instance_state']
-        for attr, value in to_return.iteritems():
-            if hasattr(value, 'to_dict'):
-                to_return[attr] = value.to_dict()
-            elif hasattr(value, '__iter__'):
-                to_return[attr] = [element.to_dict() for element in value]
+    def to_dict(self, follow_relationships=True):
+        to_return = {}
+        for column in inspect(self.__class__).mapper.columns:
+            to_return[column.name] = getattr(self, column.name)
+        relationship_names = []
+        if isinstance(self, Base):
+            relationship_names.extend([relationship_name for relationship_name in inspect(self.__class__).mapper.relationships.keys()])
+
         to_return['type'] = self.__class__.__name__
         to_return['primary_descriptor'] = self.get_primary_descriptor()
+
+        for attr in relationship_names:
+            value = getattr(self, attr)
+            if follow_relationships:
+                if hasattr(value, 'to_dict'):
+                    to_return[attr] = value.to_dict(follow_relationships = False)
+                elif hasattr(value, '__iter__'):
+                    new_list = []
+                    for element in value:
+                        if hasattr(element, 'to_dict'):
+                            new_list.append(element.to_dict(follow_relationships = False))
+                        else:
+                            new_list.append(element)
+                    to_return[attr] = new_list
+            else:
+                to_return[attr] = []
+
         return to_return
