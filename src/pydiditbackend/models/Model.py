e514c33025f8f7281eab8b2b03941ea65bf7b3e8
Adam J. Lincoln <adamjlincoln@gmail.com>
2013-12-21 04:08:31 +0000

    Save queries and work by checking follow_relationships earlier

--- src/pydiditbackend/models/Model.py
+++ src/pydiditbackend/models/Model.py
@@ -10,26 +10,23 @@ class Model(object):
         for column in inspect(self.__class__).mapper.columns:
             to_return[column.name] = getattr(self, column.name)
         relationship_names = []
-        if isinstance(self, Base):
+        if follow_relationships and isinstance(self, Base):
             relationship_names.extend([relationship_name for relationship_name in inspect(self.__class__).mapper.relationships.keys()])
 
         to_return['type'] = self.__class__.__name__
         to_return['primary_descriptor'] = self.get_primary_descriptor()
 
-        for attr in relationship_names:
+        for attr in relationship_names: # Will only run if follow_relationships is True; see above
             value = getattr(self, attr)
-            if follow_relationships:
-                if hasattr(value, 'to_dict'):
-                    to_return[attr] = value.to_dict(follow_relationships = False)
-                elif hasattr(value, '__iter__'):
-                    new_list = []
-                    for element in value:
-                        if hasattr(element, 'to_dict'):
-                            new_list.append(element.to_dict(follow_relationships = False))
-                        else:
-                            new_list.append(element)
-                    to_return[attr] = new_list
-            else:
-                to_return[attr] = []
+            if hasattr(value, 'to_dict'):
+                to_return[attr] = value.to_dict(follow_relationships = False)
+            elif hasattr(value, '__iter__'):
+                new_list = []
+                for element in value:
+                    if hasattr(element, 'to_dict'):
+                        new_list.append(element.to_dict(follow_relationships = False))
+                    else:
+                        new_list.append(element)
+                to_return[attr] = new_list
 
         return to_return
